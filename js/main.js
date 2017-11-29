15e82ae7e59da3dba452e6d7d3e51c05aade861a
SACCFFT <reck98@gmail.com>
2017-11-28 19:06:29 -0500

    updating progresson website

---
+++ js/main.js
@@ -0,0 +1,11 @@
+/* Set the width of the side navigation to 250px */
+function openNav() {
+    document.getElementById("menu-sidenav").style.width = "250px";
+    // document.getElementById("main").style.marginRight = "250px";
+}
+
+/* Set the width of the side navigation to 0 */
+function closeNav() {
+    document.getElementById("menu-sidenav").style.width = "0";
+    // document.getElementById("main").style.marginRight = "0";
+}
